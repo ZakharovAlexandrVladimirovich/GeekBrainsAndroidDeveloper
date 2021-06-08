a824e8351f0ffd2220ccf818e321c28d92ae7944
Александр <Alex362838969@gmail.com>
2021-06-09 00:46:16 +0300

    $ git config --global user.name "Zakharov Aleksandr"

---
+++ src/main/java/GeekBrainsJavaLessonsFirstQuarter/GeekBrainsLesson9/MyArrayDataException.java
@@ -0,0 +1,9 @@
+package GeekBrainsJavaLessonsFirstQuarter.GeekBrainsLesson9;
+
+public class MyArrayDataException extends Exception {
+
+
+    public MyArrayDataException(String s, int i, int j) {
+        System.err.printf("Не удалось привести значение '%s' к типу Int по индексу String[%d][%d]\n", s, i, j);
+    }
+}
---
+++ src/main/java/GeekBrainsJavaLessonsFirstQuarter/GeekBrainsLesson9/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package GeekBrainsJavaLessonsFirstQuarter.GeekBrainsLesson9;
+
+public class MyArraySizeException extends ArrayIndexOutOfBoundsException {
+    public MyArraySizeException(){
+
+        System.err.println("В метод можно передать массив только вида: String[4][]");
+    }
+}
