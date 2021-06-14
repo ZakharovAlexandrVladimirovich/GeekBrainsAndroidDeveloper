25fe580b666485dd0fbce2b77c0c2ba75521b90e
Александр <Alex362838969@gmail.com>
2021-06-14 16:58:08 +0300

    $ git config --global user.name "Zakharov Aleksandr"

---
+++ src/main/java/GeekBrainsJavaLessonsFirstQuarter/GeekBrainsLesson10/Main.java
@@ -0,0 +1,33 @@
+package GeekBrainsJavaLessonsFirstQuarter.GeekBrainsLesson10;
+
+import java.util.*;
+
+//region Урок 10. Коллекции
+//        1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
+//           Найти и вывести список уникальных слов,из которых состоит массив (дубликаты не считаем). (используй сет)
+//
+//        2. Посчитать, сколько раз встречается каждое слово. (используй мап)
+//endregion
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] fruits = {"Яблоко", "Груша", "Апельсин", "Апельсин", "Манго", "Киви", "Яблоко", "Киви", "Банан",
+                "Яблоко", "Банан", "Груша", "Лимон", "Мандарин", "Киви"};
+        HashSet<String> fruits2 = new HashSet<>();
+        Collections.addAll(fruits2, fruits);
+        System.out.println(fruits2);
+        System.out.println(fruits2.size());
+
+        HashMap<String, Integer> fruitsMap = new HashMap<>();
+
+        for (String fruit : fruits) {
+            if (fruitsMap.containsKey(fruit)) {
+                fruitsMap.put(fruit, fruitsMap.get(fruit) + 1);
+            } else {
+                fruitsMap.put(fruit, 1);
+            }
+        }
+        System.out.println(fruitsMap);
+    }
+}
---
+++ src/main/java/GeekBrainsJavaLessonsFirstQuarter/GeekBrainsLesson10/PhoneBook.java
@@ -0,0 +1,55 @@
+package GeekBrainsJavaLessonsFirstQuarter.GeekBrainsLesson10;
+import java.util.*;
+
+//        3. Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров.
+//           В этот телефонный справочник с помощью метода add() можно добавлять записи, а с помощью метода get()
+//           искать номер телефона по фамилии.Следует учесть, что под одной фамилией может быть несколько телефонов
+//           (несколько номеров у одного человека), тогда при запросе такой фамилии должны выводиться все телефоны.
+//           Нет смысла хранить для одного человека несколько одинаковых номеров.
+public class PhoneBook {
+
+    public static String name;
+    public static String number;
+    public static HashMap<Integer, String> mapBook = new HashMap<>();
+
+
+    public static void add(String name, int number) {
+        mapBook.put(number, name);
+    }
+
+    public static void get(String name) {
+
+        for (Map.Entry<Integer, String> o : mapBook.entrySet()) {
+            if (name.equals(o.getValue())) {
+                System.out.printf("Телефон %s - ",name );
+                System.out.println(o.getKey());
+            }
+        }
+    }
+
+    public static void list() {
+        for (Map.Entry<Integer, String> o : mapBook.entrySet()) {
+            System.out.printf("%s : %s\n", o.getKey(), o.getValue());
+        }
+    }
+
+    @Override
+    public int hashCode() {
+
+        return name.hashCode();
+    }
+
+    public static void main(String[] args) {
+        PhoneBook.add("Саша", 11111);
+        PhoneBook.add("Саша", 22222);
+        PhoneBook.add("Петя", 33333);
+        PhoneBook.add("Дима", 44444);
+        PhoneBook.add("Рома", 55555);
+        PhoneBook.add("Миша", 66666);
+        PhoneBook.add("Миша", 77777);
+
+        PhoneBook.list();
+        PhoneBook.get("Саша");
+
+    }
+}
