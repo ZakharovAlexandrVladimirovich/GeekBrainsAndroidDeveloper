3c967496e5ba3f7c8044547177e3b1dff5d57717
Александр <Alex362838969@gmail.com>
2021-06-20 10:26:03 +0300

    $ git config --global user.name "Zakharov Aleksandr"

--- src/main/java/GeekBrainsJavaLessonsFirstQuarter/GeekBrainsLessons__11/Main2.java
+++ src/main/java/GeekBrainsJavaLessonsFirstQuarter/GeekBrainsLessons__11/Main2.java
@@ -5,21 +5,18 @@ import java.util.Arrays;
 
 public class Main2 {
     public static void permutation(String[] strings,int a, int b){
-
         String string = strings[a];
         strings[a] = strings[b];
         strings[b] = string;
         System.out.println(Arrays.toString(strings));
     }
-    public static ArrayList<Object> arrayToList(String[] strings){
-        return new ArrayList<>(Arrays.asList((strings)));
+    public static ArrayList<Object> arrayToList(Object[] arrays){
+        return new ArrayList<>(Arrays.asList((arrays)));
     }
     public static void main(String[] args) {
 //    1. Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
 
 //    2. Написать метод, который преобразует массив в ArrayList;
-
-
 
         String[] strings = new String[]{"1", "2", "3", "4"};
         permutation(strings,0,3);
@@ -36,7 +33,6 @@ public class Main2 {
         stringArrayList.add(3,string1);
         stringArrayList.remove(4);
         System.out.println(stringArrayList);
-
 
         ArrayList<Object> objects = arrayToList(strings);
         for (Object object : objects) {
