3309e64e8b0f058d1c18603715b0002301b23641
Александр <Alex362838969@gmail.com>
2021-06-20 10:22:30 +0300

    $ git config --global user.name "Zakharov Aleksandr"

--- src/main/java/GeekBrainsJavaLessonsFirstQuarter/GeekBrainsLessons__11/Main2.java
+++ src/main/java/GeekBrainsJavaLessonsFirstQuarter/GeekBrainsLessons__11/Main2.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main2 {
-    public static void permutation(){
+   public static String[] strings = new String[]{"1", "2", "3", "4"};
+    public static void permutation(String[] strings,int a, int b){
 
-
+        String string = strings[a];
+        strings[a] = strings[b];
+        strings[b] = string;
+        System.out.println(Arrays.toString(strings));
+    }
+    public static ArrayList<Object> arrayToList(String[] strings){
+        return new ArrayList<Object>(Arrays.asList((strings)));
     }
     public static void main(String[] args) {
 //    1. Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
@@ -16,11 +23,8 @@ public class Main2 {
 
 
         String[] strings = new String[]{"1", "2", "3", "4"};
+        permutation(strings,0,3);
 
-        String string = strings[1];
-        strings[1] = strings[3];
-        strings[3] = string;
-        System.out.println(Arrays.toString(strings));
         System.out.println("**************************");
         ArrayList<String> stringArrayList = new ArrayList<>();
         stringArrayList.add("1");
@@ -32,11 +36,12 @@ public class Main2 {
         stringArrayList.remove(2);
         stringArrayList.add(3,string1);
         stringArrayList.remove(4);
-
         System.out.println(stringArrayList);
 
-//        public ArrayList<T> arrayToList(){
-//            return new ArrayList<>(Arrays.asList(this.array));
-//        }
+
+        ArrayList<Object> objects = arrayToList(strings);
+        for (Object object : objects) {
+            System.out.println(object);
+        }
     }
 }
